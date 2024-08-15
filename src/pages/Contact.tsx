57a73d1e92cbd37651db6d69e184cbb63acb9ed2
avi <heyavanindra@gmail.com>
2024-08-15 17:05:18 +0530

    optimised contact page for large screen

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,54 +2,56 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div className="text-white">
-      <div className="mt-[50px] pl-6">
-        <h3 className="text-white-800 text-6xl">Contact</h3>
+    <div className="text-white lg:flex ">
+      <div className="w-[800px] flex flex-col ">
+        <div className="mt-[50px] pl-6">
+          <h3 className="text-white-800 text-6xl">Contact</h3>
+        </div>
+        <div className="my-[10px] px-6 text-xl">
+          <p className="text-gray-600 font-serif">Some context intro stuff</p>
+        </div>
       </div>
-      <div className="my-[10px] px-6 text-xl">
-        <p className="text-gray-600 font-serif">Some context intro stuff</p>
-      </div>
-      <div className="mt-7 mx-6 text-black md:grid grid-cols-2">
-        <div>
-          <p className="p text-[#4f6dc0] text-2xl">Name</p>
+     <div className="w-full">
+     <div className="mt-7 mx-6 text-black md:grid grid-cols-2">
+        <div className="mr-6">
+          <p className="py-3 text-[#4f6dc0] text-2xl ">Name</p>
           <input
-            size={40}
             type="text"
             placeholder="Name"
-            className="h-[50px] w-50  pl-4 "
+            className="h-[40px] pl-4 w-full md:h-[50px]  md:ml-auto md:mr-5 "
           />
         </div>
-        <div>
+        <div className="md:px-4 mr-6">
           <p className="py-3  text-[#4f6dc0] text-2xl">Topic</p>
           <input
-            size={40}
             type="text"
             placeholder="Topic"
-            className="h-[50px] pl-4 w-50  "
+            className="h-[40px] pl-4 w-full md:h-[50px] md:mr-auto  rounded-"
           />
-        </div>
-        <div>
-          <p className="py-3  text-[#4f6dc0] text-2xl">Email</p>
-          <input
-            size={40}
-            type="text"
-            placeholder="Email"
-            className="h-[50px] pl-4 w-50  "
-          />
-        </div>
-        <div>
-          <p className="py-3  text-[#4f6dc0] text-2xl">Message</p>
-          <input
-            size={40}
-            type="text"
-            placeholder="Message"
-            className="h-[50px] pl-4 w-50  "
-          />
-        </div>
-        <div className="py-4">
-          <button className="bg-[#4f6dc0] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black"><Link to={'/thanks'}>Submit</Link></button>
         </div>
       </div>
+      <div className="ml-7 mr-8">
+        <p className="py-3  text-[#4f6dc0] text-2xl">Email</p>
+        <input
+          type="text"
+          placeholder="Email"
+          className="h-[50px] pl-4 w-full  text-black"
+        />
+      </div>
+      <div className="ml-7 mr-8">
+        <p className="py-3  text-[#4f6dc0] text-2xl">Message</p>
+        <input
+          type="text"
+          placeholder="Message"
+          className="h-[50px] pl-4 w-full  text-black "
+        />
+      </div>
+      <div className="py-4 mx-8">
+        <button className="bg-[#4f6dc0] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          <Link to={"/thanks"}>Submit</Link>
+        </button>
+      </div>
+     </div>
     </div>
   );
 };
